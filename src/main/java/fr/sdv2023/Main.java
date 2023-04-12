e1d9a8dbef16600a899e7a9178feac21a8de6290
Isaatox <hugo35340@gmail.com>
2023-04-12 16:28:42 +0200

    First Commit

--- src/main/java/fr/sdv2023/Main.java
+++ src/main/java/fr/sdv2023/Main.java
@@ -1,7 +1,25 @@
 package fr.sdv2023;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Créer une instance d'EntityManagerFactory
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("demo");
+
+        // Créer une instance d'EntityManager
+        EntityManager em = emf.createEntityManager();
+
+        // Vérifier la connexion à la base de données
+        if (em.isOpen()) {
+            System.out.println("Connecté à la bdd");
+        } else {
+            System.out.println("Erreur de connexion à la bdd");
+        }
+
+        // Fermer la connexion
+        em.close();
+        emf.close();
     }
 }
